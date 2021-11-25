bec34f85907f482249e4be526b490deef5afa104
rouillonh <gaxf100@hotmail.com>
2021-11-25 04:55:52 -0500

    Gramification

---
+++ p1_rouillonh.py
@@ -0,0 +1,5 @@
+x = "Sala Hipersueño"
+print(x[0:2])
+print(x[12:15])
+print(x[0:15:2])
+print(x+x[ : :-1])
---
+++ p2_rouillonh.py
@@ -0,0 +1,8 @@
+cadena = "Mapa3"
+c = ','
+x = list(cadena)
+y = " ".join(x)
+
+print(y.replace(" ",c))
+
+
